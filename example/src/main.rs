2fc17fc63106d83d81739c83d0076c2aa9e32c29
Gary Guo <gary@garyguo.net>
2021-08-26 11:06:45 +0100

    Add an example

---
+++ example/src/main.rs
@@ -0,0 +1,52 @@
+#![no_std]
+#![feature(start)]
+#![feature(default_alloc_error_handler)]
+
+extern crate alloc;
+extern crate unwind;
+
+use alloc::{borrow::ToOwned, string::String};
+use unwind::print::*;
+
+#[link(name = "c")]
+extern "C" {}
+
+struct PrintOnDrop(String);
+
+impl Drop for PrintOnDrop {
+    fn drop(&mut self) {
+        println!("dropped: {:?}", self.0);
+    }
+}
+
+struct PanicOnDrop;
+
+impl Drop for PanicOnDrop {
+    fn drop(&mut self) {
+        panic!("panic on drop");
+    }
+}
+
+fn foo() {
+    panic!("panic");
+}
+
+fn bar() {
+    let _p = PrintOnDrop("string".to_owned());
+    foo()
+}
+
+#[start]
+fn main(_argc: isize, _argv: *const *const u8) -> isize {
+    unwind::panic::catch_unwind(|| {
+        let _ = unwind::panic::catch_unwind(|| {
+            bar();
+            println!("done");
+        });
+        println!("caught");
+        let _p = PanicOnDrop;
+        foo();
+        0
+    })
+    .unwrap_or(101)
+}

96888d814ff64cfe0ba2cda46dfb7606238d873b
Gary Guo <gary@garyguo.net>
2023-12-30 21:31:25 +0000

    core_intrinsics is now also marked as internal feature, so fix the warning

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,12 @@
 // lang_items is an internal feature. `internal_features` lint is added recently
 // so also allow unknown lints to prevent warning in older nightly versions.
 #![cfg_attr(
-    any(feature = "personality", feature = "personality-dummy"),
+    any(
+        feature = "personality",
+        feature = "personality-dummy",
+        feature = "panicking",
+        feature = "panic-handler-dummy"
+    ),
     allow(internal_features)
 )]
 #![cfg_attr(

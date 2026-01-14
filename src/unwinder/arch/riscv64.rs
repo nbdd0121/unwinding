79d0ea19809223a5187953ed5d94f1ed955aae81
Taiki Endo <te316e89@gmail.com>
2026-01-14 23:40:58 +0900

    Support RV32E

--- src/unwinder/arch/riscv64.rs
+++ src/unwinder/arch/riscv64.rs
@@ -10,6 +10,9 @@ pub const MAX_REG_RULES: usize = 65;
 #[cfg(all(target_feature = "f", not(target_feature = "d")))]
 compile_error!("RISC-V RV64 with only F extension is not supported");
 
+#[cfg(target_feature = "e")]
+compile_error!("RISC-V RV64E is not supported");
+
 #[repr(C)]
 #[derive(Clone, Default)]
 pub struct Context {

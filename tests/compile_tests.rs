0cedf1793e83f44b1718d65bb3f2b24947cc3e9c
Gary Guo <gary@garyguo.net>
2026-08-16 13:47:25 +0100

    Rework test_crates

--- tests/compile_tests.rs
+++ tests/compile_tests.rs
@@ -4,17 +4,12 @@ use std::process::Command;
 fn main() {
     let dir = env!("CARGO_MANIFEST_DIR");
 
-    let tests = [
-        "throw_and_catch",
-        "catch_std_exception",
-        "std_catch_exception",
-        "panic_abort_no_debuginfo",
-        "forced_unwind",
-    ];
+    let tests = std::fs::read_dir(format!("{dir}/test_crates")).unwrap();
 
     for test in tests {
+        let test = test.unwrap();
         let status = Command::new("./check.sh")
-            .current_dir(format!("{dir}/test_crates/{test}"))
+            .current_dir(test.path())
             .status()
             .unwrap();
         assert!(status.success());
